450e47d1db26cdabc0a518e395ba9d8d5592add6
tolache <anatoly.cherenkov@gmail.com>
2020-04-24 12:42:46 +0300

    add sayHi()

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -1,17 +1,21 @@
 package com.example.helloworld;
 
-public class HelloWorld {
-    public static void main(String[] args) {
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
         sayHello();
         sayHi();
     }
 
     @Deprecated
-    private static void sayHello() {
+    private static void sayHello()
+    {
         System.out.println("Hello, World!");
     }
 
-    private static void sayHi() {
+    private static void sayHi()
+    {
         System.out.println("Hi, World!");
     }
 }
